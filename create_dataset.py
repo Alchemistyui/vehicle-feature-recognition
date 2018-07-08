a93179f5b74bb179c5ebaa11d4a199704bda96b9
Alchemist <1319525622@qq.com>
2018-07-08 10:40:14 +0800

    finish dct & idct

--- create_dataset.py
+++ create_dataset.py
@@ -47,8 +47,8 @@ def load_h5(file_dir):
     train_set_x_orig = np.array(dataset['X_train'][:]) # your train set features
     test_set_x_orig = np.array(dataset['X_test'][:]) # your train set features
     dataset.close()
-    print(train_set_x_orig.shape)
-    print(test_set_x_orig.shape)
+    # print(train_set_x_orig.shape)
+    # print(test_set_x_orig.shape)
     #测试
     # plt.imshow(train_set_x_orig[2])
     # pylab.show()
@@ -56,10 +56,12 @@ def load_h5(file_dir):
 def load_picture(image_list):
     imgs = [];
     for i in range(len(image_list)):
-        img = cv2.imread(image_list[i])
-        dst = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        imgs.append(dst)
+        img = cv2.imread(image_list[i], 0)
+        # dst = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        imgs.append(img)
     # 测试
+    io.imshow(imgs[2])
+    io.show()
     # for i in range(len(imgs)):
     #     cv2.imshow('image',imgs[i])
     #     cv2.waitKey(5)
@@ -74,15 +76,19 @@ def dct(cv2imgs):
         img = cv2imgs[i].astype('float')
         img_dct = cv2.dct(img)    
         img_dct_log = np.log(abs(img_dct))  #进行log处理
-        imgs.append(img_dct_log)
-        imgs_log.append(img_dct)
+        imgs.append(img_dct)
+        imgs_log.append(img_dct_log)
     # 测试
+    # io.imshow(cv2imgs[2].astype('float'))
+    # io.show()
+    # io.imshow(imgs[2])
+    # io.show()
     # cv2.imshow('image2',cv2imgs[2].astype('float'))
     # cv2.waitKey(0)
     # for i in range(len(cv2imgs)):
     #     cv2.imshow('image',imgs[i])
     #     cv2.waitKey(10)
-    cv2.destroyAllWindows()
+    # cv2.destroyAllWindows()
     return imgs,imgs_log
 
 def idct(img_dct):
@@ -91,6 +97,14 @@ def idct(img_dct):
         img_recor = cv2.idct(img_dct[i])
         imgs.append(img_recor) 
     # 测试
+    # io.imshow(imgs[2])
+    # io.show()
+
+    # plt.subplot(236)
+    plt.imshow(imgs[2],'gray')
+    plt.title('IDCT2(cv2_idct)')
+
+    plt.show()
     # cv2.imshow('image2',imgs[2])
     # cv2.waitKey(0)
     # for i in range(len(img_dct)):
@@ -109,12 +123,13 @@ def sign(img_dct_log):
 
 
 def test(cv2imgs):
-    img = cv2.imread(image_list[2])
+    img = cv2.imread(image_list[2], 0)
     img1 = img.astype('float')
-    dst = color.rgb2gary(img)
-    print(dst.dtype)
-    io.imshow(dst)
-    io.show()  
+    # dst = color.rgb2gary(img)
+    # img1 = io.imread(image_list[2], 1)
+    print(img1.dtype)
+    # io.imshow(dst)
+    # io.show()  
 
     img_dct = cv2.dct(img1)         #进行离散余弦变换
      
@@ -122,10 +137,17 @@ def test(cv2imgs):
      
     img_recor = cv2.idct(img_dct)    #进行离散余弦反变换
 
-    cv2.imshow('image3',img)
-    cv2.waitKey(0)
-    cv2.imshow('image2',img1)
-    cv2.waitKey(0)
+    io.imshow(img_dct)
+    io.show()
+    # io.imshow(img_dct_log)
+    # io.show()
+    io.imshow(img_recor)
+    io.show()
+
+    # cv2.imshow('image3',img)
+    # cv2.waitKey(0)
+    # cv2.imshow('image2',img1)
+    # cv2.waitKey(0)
     # cv2.imshow('image',img_recor)
     # cv2.waitKey(0)
     # cv2.imshow('image1',img_dct)
@@ -175,8 +197,8 @@ img_dct, img_dct_log = dct(cv2imgs)
 sign_img = sign(img_dct_log)
 gauss_img = GaussianBlur(sign_img)
 # normal = Normalize(gauss_img)
-# idct_img = idct(sign_img)
-test(cv2imgs)
+idct_img = idct(img_dct)
+# test(cv2imgs)
 # print(len(label_list))
 
 
