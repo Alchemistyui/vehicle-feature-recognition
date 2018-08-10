4469b037c419af90b02ca9e51e88a91ec9a44bd1
Alchemist <1319525622@qq.com>
2018-08-10 13:01:50 +0800

    finish make dataset

--- search_bing_api.py
+++ search_bing_api.py
@@ -23,7 +23,7 @@ args = vars(ap.parse_args())
 # set your Microsoft Cognitive Services API key along with (1) the
 # maximum number of results for a given search and (2) the group size
 # for results (maximum of 50 per request)
-API_KEY = "YOUR_API_KEY_GOES_HERE"
+API_KEY = "38ced9e874324b45884a1b68d41e837a"
 MAX_RESULTS = 250
 GROUP_SIZE = 50
 
@@ -40,17 +40,19 @@ EXCEPTIONS = set([IOError, FileNotFoundError,
 
 # store the search term in a convenience variable then set the
 # headers and search parameters
+# 初始化搜索参数
 term = args["query"]
 headers = {"Ocp-Apim-Subscription-Key" : API_KEY}
 params = {"q": term, "offset": 0, "count": GROUP_SIZE}
 
-# make the search
+# make the search搜索
 print("[INFO] searching Bing API for '{}'".format(term))
 search = requests.get(URL, headers=headers, params=params)
 search.raise_for_status()
 
 # grab the results from the search, including the total number of
 # estimated results returned by the Bing API
+# 执行搜索并以JSON格式获取结果，计算并向终端打印估计的结果数量，初始化总数。
 results = search.json()
 estNumResults = min(results["totalEstimatedMatches"], MAX_RESULTS)
 print("[INFO] {} total results for '{}'".format(estNumResults,
@@ -73,10 +75,12 @@ for offset in range(0, estNumResults, GROUP_SIZE):
         offset, offset + GROUP_SIZE, estNumResults))
 
     # loop over the results
+    # 循环遍历当前批量的图像，并尝试将每个单独的图像下载到我们的输出文件夹。
     for v in results["value"]:
         # try to download the image
         try:
             # make a request to download the image
+            # 尝试通过URL获取图像，并为其构建路径+文件名，然后我们尝试打开并将文件写入磁盘
             print("[INFO] fetching: {}".format(v["contentUrl"]))
             r = requests.get(v["contentUrl"], timeout=30)
 
@@ -100,6 +104,7 @@ for offset in range(0, estNumResults, GROUP_SIZE):
                 continue
 
         # try to load the image from disk
+        # 看看OpenCV是否可以实际加载图像，这意味着（1）图像文件已成功下载，（2）图像路径有效：
         image = cv2.imread(p)
 
         # if the image is `None` then we could not properly load the
@@ -111,3 +116,7 @@ for offset in range(0, estNumResults, GROUP_SIZE):
 
         # update the counter
         total += 1
+
+
+# mkdir dataset/yui
+# python3 search_bing_api.py --query "yui yoshioka" --output dataset/yui
